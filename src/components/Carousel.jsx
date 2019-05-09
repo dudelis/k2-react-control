2266ae50d37b9fb3b320b45bf064af65fece78b4
Konstantin Fukszon <fukszon.kostya@gmail.com>
2019-05-09 05:57:00 -0700

    Add carousel component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+
+export default class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageData: []
+    };
+  }
+  componentDidMount() {
+    //1. Make an OData call and get all the images
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://k2.denallix.com/api/odata/v3/FileTests");
+    SourceCode.Forms.XSRFHelper.setAntiXSRFHeader(xhr);
+    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.send();
+    xhr.onload = function() {
+      if (xhr.status != 200) {
+        // analyze HTTP status of the response
+        alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+      } else {
+        // show the result
+        console.log(xhr.response);
+        this.setState({ imageData: xhr.response.value });
+      }
+    };
+  }
+  render() {
+    //1. Getting custom elements
+    const childImages = this.state.imageData.map(function(element) {
+      return (
+        <div>
+          <img src={element["FileProperty@odata.mediaReadLink"]} />
+          <p className="legend">{element.Name}</p>
+        </div>
+      );
+    });
+
+    return (
+        <Carousel>
+            {childImages}
+        </Carousel>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/MasonryLayout';
 
 
 ReactDOM.render(<App />, document.getElementsByName('react-control-root')[0]);
